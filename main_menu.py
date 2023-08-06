7843f91bf64b9ae3f8bc915057ffa175c9eefbc6
Yurii-Potapchuk <130280920+Yurii-Potapchuk@users.noreply.github.com>
2023-08-06 11:51:47 +0200

    Новий скрипт меню з оновленим візуалом

---
+++ main_menu.py
@@ -0,0 +1,44 @@
+""""This is main Menu"""
+
+from sort import sorter_starter
+from addressbook import addressbook_starter # функція адресної книжки заглушка
+from notebook import notes_starter #функція записника заглушка
+
+def menu():
+
+    COMMANDS = ["1 - AddressBook", "2 - NoteBook", "3 - Files sorter", "0 - Exit"]
+
+    while True:
+
+        print("_"*34)
+        print("|{:^32}|".format("Welcome to main menu"))
+        print('|'+'_'*32 + '|')
+        for el in COMMANDS:
+            print('|{:<32}|'.format(el))
+        print('|'+'_'*32 + '|')
+        print('|{:<32}|'.format('Type number to start:  '))
+        user_input = input("|>>> ")
+
+        if user_input == '1':
+            print("_"*34)
+            print("|{:^32}|".format("AddressBook Started!"))
+    
+            addressbook_starter()
+        elif user_input == '2':
+            print("_"*34)
+            print("|{:^32}|".format("NoteBook Started!"))
+    
+            notes_starter()
+        elif user_input == '3':
+            print("_"*34)
+            print("|{:^32}|".format("Files Sorter Started!"))
+
+            result = sorter_starter()
+            print(result)
+        elif user_input == '0':
+            print('Goodbye!')
+            break
+
+
+if __name__ == '__main__':
+    menu()
