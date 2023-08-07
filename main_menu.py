26f773940a5ccc0b6458c1f4fb03a70b9359e1d7
Rostyslav Bysko <132708200+RostislavB74@users.noreply.github.com>
2023-08-07 20:11:01 +0300

    addresbook connected

--- main_menu.py
+++ main_menu.py
@@ -1,36 +1,38 @@
 """"This is main Menu"""
 
 from sort import sorter_starter
-from addressbook import addressbook_starter #заглушка адресбука
-from my_Notes import notes_main as notes_starter  
+from addressbook import addressbook_starter
+from my_Notes import notes_main as notes_starter
+
 
 def menu():
 
-    COMMANDS = [" 1  - AddressBook📒", " 2  - NoteBook📋", " 3  - Files sorter📂", " 0  - Exit❌"]
+    COMMANDS = [" 1  - AddressBook📒", " 2  - NoteBook📋",
+                " 3  - Files sorter📂", " 0  - Exit❌"]
 
     while True:
 
         print("_"*34)
-        print("| {:<3} {:^27}|".format("☰","Welcome to main menu"))
+        print("| {:<3} {:^27}|".format("☰", "Welcome to main menu"))
         print('|'+'_'*32 + '|')
         for el in COMMANDS:
             print('|{:<31}|'.format(el))
         print('|'+'_'*32 + '|')
         print('|{:<32}|'.format('Type number to start:  '))
-        user_input = input("|>>> " )
+        user_input = input("|>>> ")
         print('|'+'_'*32 + '|')
 
         if user_input == '1':
             print("_"*34)
             print("|{:^30}|".format("✨ AddressBook Started! ✨"))
             print("|"+"_"*32 + "|")
-    
+
             addressbook_starter()
         elif user_input == '2':
             print("_"*34)
             print("|{:^30}|".format("✨ NoteBook Started! ✨"))
             print("|"+"_"*32 + "|")
-    
+
             notes_starter()
         elif user_input == '3':
             print("_"*34)
@@ -39,7 +41,7 @@ def menu():
 
             result = sorter_starter()
             print(result)
-        elif user_input == '0' or user_input.lower() =="exit":
+        elif user_input == '0' or user_input.lower() == "exit":
             print('Goodbye!')
             break
         else:
