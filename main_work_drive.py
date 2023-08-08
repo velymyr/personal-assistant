9017af4662e1644d12a89f8915f6b1cb343574fe
Volodymyr Hladkyi <volodymyr.hladkyi@gmail.com>
2023-08-08 22:36:38 +0300

    simplify regex

--- main_work_drive.py
+++ main_work_drive.py
@@ -92,7 +92,7 @@ def check_bd(args):
 
 
 def check_email(args):
-    pattern = r"^[-a-z0-9!#$%&'+/=?^_{|}~]+(?:\.[-a-z0-9!#$%&'*+/=?^_{|}~]+)@(?:a-z0-9?.)*(?:aero|arpa|asia|biz|cat|com|coop|edu|gov|info|int|jobs|mil|mobi|museum|name|net|org|pro|tel|travel|[a-z][a-z])$"
+    pattern = r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
     if re.match (pattern, args):
         email = Email(args)
     print(email)
