69ac3f3d70bcf673e9d610120c914741e0510180
Volodymyr Hladkyi <volodymyr.hladkyi@gmail.com>
2023-08-09 18:54:44 +0300

    typo, tabs (#42)

--- notes.py
+++ notes.py
@@ -8,113 +8,114 @@ from rich.table import Table
 class Tag:
 
     def __init__(self, value):
-      self.value = value
-    
+        self.value = value
+
     def __str__(self):
-      return self.value
+        return self.value
 
     def __repr__(self):
-      return f"{self.value}"
+        return f"{self.value}"
 
     def __getstate__(self):
-      return self.value
+        return self.value
 
     def __setstate__(self, state):
-      self.value = state
+        self.value = state
 
 
 class Tags:
 
     def __init__(self):
-      self.tags = []
+        self.tags = []
 
     def __str__(self):
-      return ", ".join(str(tag) for tag in self.tags)
+        return ", ".join(str(tag) for tag in self.tags)
 
     def __repr__(self):
-      return f"{self.tags}"
+        return f"{self.tags}"
 
     def __getstate__(self):
-      return self.tags
+        return self.tags
 
     def __setstate__(self, state):
-      self.tags = state
+        self.tags = state
 
     def __iter__(self):
-          return iter(self.tags)
-    
-  
+        return iter(self.tags)
+
+
 class Note:
 
     def __init__(self, note_text):
-      self.note_text = note_text
+        self.note_text = note_text
 
     def __str__(self):
-      return self.note_text
+        return self.note_text
 
     def __repr__(self):
-      return f"{self.note_text}"
+        return f"{self.note_text}"
 
     def __getstate__(self):
-      return self.note_text
+        return self.note_text
 
     def __setstate__(self, state):
-      self.note_text = state
+        self.note_text = state
 
 
 class NoteBook(UserDict):
 
     def add_note(self, note, tags):
-      self.data[note] = tags
+        self.data[note] = tags
 
     def save(self, filename='notebook_data.pkl'):
-      with open(filename, 'wb') as f:
-        pickle.dump(self.data, f)
+        with open(filename, 'wb') as f:
+            pickle.dump(self.data, f)
 
     def load(self, filename='notebook_data.pkl'):
-      try:
-        with open(filename, 'rb') as f:
-          data = pickle.load(f)
-          if not isinstance(data, dict):
-            raise TypeError('Invalid data type') 
-          self.data = data
-      except (FileNotFoundError, TypeError):
-        self.data = {}
+        try:
+            with open(filename, 'rb') as f:
+                data = pickle.load(f)
+                if not isinstance(data, dict):
+                    raise TypeError('Invalid data type')
+                self.data = data
+        except (FileNotFoundError, TypeError):
+            self.data = {}
 
     def show_notes(self):
-            n = 1
-            console = Console()
-            table = Table(show_header=True, header_style="bold magenta", width=60,show_lines=True)
-            table.add_column("#", max_width= None)
-            table.add_column("Note", width= 20, no_wrap=False)
-            table.add_column("Tags")
-            
-            for key, tags in self.data.items():
-                table.add_row(str(n), str(key), ", ".join(str(t) for t in self.data[key]))
-                n += 1
-            
-            console.print(table)
+        n = 1
+        console = Console()
+        table = Table(show_header=True, header_style="bold magenta", width=60, show_lines=True)
+        table.add_column("#", max_width=None)
+        table.add_column("Note", width=20, no_wrap=False)
+        table.add_column("Tags")
+
+        for key, tags in self.data.items():
+            table.add_row(str(n), str(key), ", ".join(str(t) for t in self.data[key]))
+            n += 1
+
+        console.print(table)
 
     def edit_note(self):
         print("\n***Edit func***")
         self.show_notes()
-        
+
         x = input("***Notebook says***\nChoose the note you want to edit by number ('0' - to exit delete func):\n>>> ")
-        
+
         try:
             x = int(x)
             keys = list(self.data.keys())
             if 1 <= x <= len(keys):
                 note_to_edit = keys[x - 1]
                 new_note = input(f"***Notebook says***\nEnter the new content for note '{note_to_edit}': ")
-                new_tags = input(f"***Notebook says***\nEnter the new tags for note '{note_to_edit}' (comma-separated): ").split(",")
-                
+                new_tags = input(
+                    f"***Notebook says***\nEnter the new tags for note '{note_to_edit}' (comma-separated): ").split(",")
+
                 self.data[new_note] = [tag.strip() for tag in new_tags]
                 if note_to_edit != new_note:
                     del self.data[note_to_edit]
                 print(f"***Notebook says***\nNote '{note_to_edit}' has been updated.")
             elif x == 0:
-               return 'Exit "Edit func" success'
+                return 'Exit "Edit func" success'
             else:
                 print("\n***Ooops***\nInvalid input. Please choose a valid number.")
                 nb.edit_note()
@@ -127,15 +128,15 @@ class NoteBook(UserDict):
         for key, value in self.items():
             if str(key).find(text) != -1:
                 found_fields.append((key, value))
-        
+
         console = Console()
         table = Table(show_header=True, header_style="bold magenta", width=60, show_lines=True)
         table.add_column("Note", width=20, no_wrap=False)
         table.add_column("Tags")
-        
+
         for obj in found_fields:
             table.add_row(str(obj[0]), str(obj[1]))
-        
+
         if found_fields:
             return console.print(table)
         else:
@@ -143,43 +144,43 @@ class NoteBook(UserDict):
 
     def search_tag(self, text):
         found_tags = []
-        
+
         for key, value in self.items():
             tag_lst = ', '.join(str(v) for v in value)
             if text in tag_lst:
                 found_tags.append((str(key), tag_lst))
-        
+
         console = Console()
         table = Table(show_header=True, header_style="bold magenta", width=60, show_lines=True)
         table.add_column("Key", width=20, no_wrap=False)
         table.add_column("Tags")
-        
+
         for obj in found_tags:
             table.add_row(str(obj[0]), str(obj[1]))
-        
+
         if found_tags:
             return console.print(table)
         else:
             return console.print("\n***Ooops***\nNo matching found.")
 
-    
+
 nb = NoteBook()
 
 
 def add_note():
     user_input_note = input('\n***Add func***\nInput your note ("0" - to exit delete func):\n>>>')
     if user_input_note == "0":
-       return 'Exit "Add func" success'
+        return 'Exit "Add func" success'
     elif not user_input_note:
-       print("\nEmpty note not allowed")
-       add_note()
+        print("\nEmpty note not allowed")
+        add_note()
     else:
         user_input_tags = input('***Notebook says***\nInput tags for a note (space-separated):\n>>>')
         user_input_tags = user_input_tags.strip().split()
         tags = Tags()
         for user_tag in user_input_tags:
             tag = Tag(user_tag)
-            
+
             tags.tags.append(tag)
         note = Note(user_input_note)
         nb.add_note(note, tags)
@@ -187,29 +188,29 @@ def add_note():
 
 
 def delete_note():
-        nb.show_notes()
-        
-        x = input("\n***Delete func***\nChoose the note you want to delete by number ('0' - to exit delete func):\n>>> ")
-        
-        try:
-            x = int(x)
-            keys = list(nb.data.keys())
-            if 1 <= x <= len(keys):
-                note_to_delete = keys[x - 1]
-                del nb.data[note_to_delete]
-                print(f"Note '{note_to_delete}' has been deleted.")
-            elif x == 0:
-               return 'Exit "Delete func" success'
-            else:
-                print("\n***Ooops***\nInvalid input. Please enter a valid number.")
-                delete_note()
-        except ValueError:
-            print("\n***Ooops***\nInvalid input. Please enter a number.")
+    nb.show_notes()
+
+    x = input("\n***Delete func***\nChoose the note you want to delete by number ('0' - to exit delete func):\n>>> ")
+
+    try:
+        x = int(x)
+        keys = list(nb.data.keys())
+        if 1 <= x <= len(keys):
+            note_to_delete = keys[x - 1]
+            del nb.data[note_to_delete]
+            print(f"Note '{note_to_delete}' has been deleted.")
+        elif x == 0:
+            return 'Exit "Delete func" success'
+        else:
+            print("\n***Ooops***\nInvalid input. Please enter a valid number.")
             delete_note()
+    except ValueError:
+        print("\n***Ooops***\nInvalid input. Please enter a number.")
+        delete_note()
 
 
 def change_note():
-   return nb.edit_note()
+    return nb.edit_note()
 
 
 def exit_notes():
@@ -225,14 +226,15 @@ def search():
     if user_choice == "1" or user_choice == "2":
         search_key = input("Enter a search keyword\n>>>")
         if user_choice == '1':
-          return nb.search_note(search_key)
+            return nb.search_note(search_key)
         elif user_choice == '2':
             return nb.search_tag(search_key)
         else:
-          return "Wrong input"
+            return "Wrong input"
     else:
-       print("\n***Ooopd***\nWrong input")
-       search()
+        print("\n***Ooops***\nWrong input")
+        search()
+
 
 def menu():
     pass
@@ -257,25 +259,25 @@ def pars(txt_comm: str, command_dict):
     return command
 
 
-def command_handler(user_input, note_commands):
-    if user_input in note_commands:
-        return note_commands[user_input][0]()
-    possible_command = difflib.get_close_matches(user_input, note_commands, cutoff=0.5)
+def command_handler(user_input, commands):
+    if user_input in commands:
+        return commands[user_input][0]()
+    possible_command = difflib.get_close_matches(user_input, commands, cutoff=0.5)
     if possible_command:
         return f'Wrong command. Maybe you mean: {", ".join(possible_command)}'
     else:
         return f'Wrong command.'
-    
+
 
 def instruction(command_dict):
     console = Console()
     table = Table(show_header=True, header_style="bold magenta", width=60, show_lines=False)
-    table.add_column("Command", max_width= None, no_wrap=False)
-    table.add_column("Description", width= 20, no_wrap=False)
+    table.add_column("Command", max_width=None, no_wrap=False)
+    table.add_column("Description", width=20, no_wrap=False)
 
     for func_name, func in command_dict.items():
         table.add_row(str(func_name), str(func[1]))
-    
+
     console.print(table)
 
 
@@ -293,7 +295,7 @@ def notes_main():
             print('Notebook closed')
             break
         elif user_input_command == 'show all':
-           show_notes()
+            show_notes()
         else:
             if command in note_commands:
                 result = command_handler(command, note_commands)
@@ -301,8 +303,7 @@ def notes_main():
                 result = command_handler(user_input_command, note_commands)
             nb.save()
             if result:
-               print(result)
-
+                print(result)
 
 
 if __name__ == "__main__":
