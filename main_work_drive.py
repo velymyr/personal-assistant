e8524077701408c2ef698752af67dee995380667
Rostyslav Bysko <brn.h74@gmail.com>
2023-08-09 15:25:22 +0300

    ok

--- main_work_drive.py
+++ main_work_drive.py
@@ -8,7 +8,7 @@ filename = 'address_book'
 
 def input_error(func):
     def wrapper(*args, **kwargs):
-        error_mgs = 'Give me a name and phone number in format +380(88)777-77-77 or date birthday dd/mm/YYYY'
+        error_mgs = 'Give me correct parameters command+name--> enter'
         try:
             return func(*args, **kwargs)
         except UnboundLocalError as e:
@@ -27,46 +27,6 @@ def input_error(func):
     return wrapper
 
 
-# @input_error
-# def add_contact(*args):
-#     bd = None
-#     name = Name(args[0])
-#     list_phones = []
-#     list_emails = []
-#     rec: Record = address_book.get(str(name))
-#     if rec:
-#         for i in range(1, len(args)):
-#             print(args[i])
-#             if not rec.birthday:
-#                 bd = check_bd(args[i])
-#                 if bd:
-#                     return rec.add_birthday(bd)
-#             if check_phone(args[i]):
-#                 # if phone:
-#                 list_phones.append(rec.add_phone(args[i]))
-#                 return list_phones
-#             if check_email(args[i]):
-#                 # if email:
-#                 list_emails.append(rec.add_email(args[i]))
-#                 return list_emails
-#         # else:
-#         #     return "Unknown command"
-#     if not rec:
-#         for i in range(1, len(args)):
-#             bd = check_bd(args[i])
-#             birthday = bd
-#             if check_phone(args[i]):
-#                 # if phone:
-#                 list_phones.append(args[i])
-
-#             if check_email(args[i]):
-#                 list_emails.append(args[i])
-
-#         rec = Record(name, phone=list_phones,
-#                      birthday=birthday, email=list_emails)
-#         return address_book.add_record(rec)
-#     else:
-#         return "Unknown command"
 
 
 @input_error
@@ -80,47 +40,50 @@ def change_phone(*args):
     return f"No contact {name} in address book"
 
 
-def check_bd(args):
-    pattern_bd = r'(\d\d)/(\d\d)/(\d{4})'
-    if re.fullmatch(pattern_bd, args):
-        data = Birthday(args)
-        if isinstance(data.value, datetime):
-            birthday = data
-    else:
-        birthday = None
-    return birthday
+# def check_bd(args):
+#     pattern_bd = r'(\d\d)/(\d\d)/(\d{4})'
+#     if re.fullmatch(pattern_bd, args):
+#         data = Birthday(args)
+#         if isinstance(data.value, datetime):
+#             birthday = data
+#     else:
+#         birthday = None
+#     return birthday
 
 
-def check_email(args):
-    pattern = r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
-    if re.match(pattern, args):
-        email = Email(args)
-    print(email)
-    return email
+# def check_email(args):
+#     pattern = r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
+#     if re.match(pattern, args):
+#         email = Email(args)
+#     print(email)
+#     return email
 
 
-def check_phone(args):
-    pattern_ph = r"(\+\d{3}\(\d{2}\)\d{3}\-(?:(?:\d{2}\-\d{2})|(?:\d{1}\-\d{3}){1}))"
-    if re.fullmatch(pattern_ph, args):
-        phone = Phone(args)
-    else:
-        phone = None
-    print(phone)
-    return phone
+# def check_phone(args):
+#     pattern_ph = r"(\+\d{3}\(\d{2}\)\d{3}\-(?:(?:\d{2}\-\d{2})|(?:\d{1}\-\d{3}){1}))"
+#     if re.fullmatch(pattern_ph, args):
+#         phone = Phone(args)
+#     else:
+#         phone = None
+#     print(phone)
+#     return phone
 
-# Видалити запис
 
-#@input_error
 
+@input_error
 def edit(name, parameter, new_value):
     res: Record = address_book.get(str(name))
-    print(res)
+    #print(res)
     try:
         if res:
             if parameter == 'birthday':
                 new_value = Birthday(new_value).value
             elif parameter == 'email':
-                new_value = Email(new_value).value
+                parameter= 'emailes'
+                new_contact = new_value.split(' ')
+                new_value = []
+                for emailes in new_contact:
+                        new_value.append(Email(emailes).value)
             elif parameter == 'address':
                 new_value = Address(new_value).value
             elif parameter == 'note':
@@ -132,12 +95,8 @@ def edit(name, parameter, new_value):
                         new_value.append(Phone(number).value)
             if parameter in res.__dict__.keys():
                 res.__dict__[parameter] = new_value
-            # if parameter in :
-            #     rec[parameter] = new_value
-            # else:
-            #     raise ValueError
-        # if name not in res:
-        #raise NameError
+        res: Record = address_book.get(str(name))
+        print(res)   
     except ValueError:
         print('Incorrect parameter! Please provide correct parameter')
     except NameError:
@@ -145,6 +104,7 @@ def edit(name, parameter, new_value):
     
 
 
+# Видалити запис
 @input_error
 def delete_record(*args):
     name = Name(args[0])
@@ -154,9 +114,9 @@ def delete_record(*args):
     return f"No contact '{name}' found in the address book."
 
 
-# Вийти
-def exit_command(*args):
-    return "Good bye!"
+# # Вийти
+# def exit_command(*args):
+#     return "Good bye!"
 
 
 # коли день народження
@@ -172,8 +132,6 @@ def get_days_to_birthday(*args):
     return f'{name} until the next birthday left {result} days'
 
 # показати контакт
-
-
 @input_error
 def get_phone(*args):
     name = Name(args[0])
@@ -182,15 +140,15 @@ def get_phone(*args):
     return f"{res.name} : {result}"
 
 
-# Привіт
-def hello(*args):
-    return "How can I help you?"
+# # Привіт
+# def hello(*args):
+#     return "How can I help you?"
 
-# Невідома команда пуста команда
+# # Невідома команда пуста команда
 
 
-def no_command(*args, **kwargs):
-    return "Unknown command"
+# def no_command(*args, **kwargs):
+#     return "Unknown command"
 
 
 # Видалити телефон
@@ -198,7 +156,6 @@ def no_command(*args, **kwargs):
 def remove_phone(*args):
     name = Name(args[0])
     phone = Phone(args[1])
-
     rec: Record = address_book.get(str(name))
     print(rec)
     if rec:
@@ -233,8 +190,6 @@ def search_record(*args):
     return address_book_search
 
 # показати все
-
-
 @input_error
 def show_all_command(*args):
     if Record.__name__:
@@ -242,26 +197,3 @@ def show_all_command(*args):
     # return
 
 
-# Команди додати, змінити, видалити телефон, вихід, показати все, показати контакт
-# COMMANDS = {
-#     exit_command: ("good bye", "bye", "exit", "end", "close", "quit", "0"),
-#     add_contact: ("add ", "+ ", "1"),
-#     change_phone: ("change ", "зміни ", "2"),
-#     remove_phone: ("remove ", "delete ", "del ", "-", "3"),
-#     show_all_command: ("show all", "show", "4"),
-#     get_phone: ("phone ", "5"),
-#     get_days_to_birthday: ("birthday", "bd", "6"),
-#     delete_record: ("7"),
-#     # add_note: ('note', 'нотаток'),
-#     search_record: ('search', "find", '8'),
-#     hello: ("hello", "hi", "!",)
-# }
-
-
-# def parser(text: str):
-#     for cmd, kwds in COMMANDS.items():
-#         for kwd in kwds:
-#             if text.lower().startswith(kwd):
-#                 data = text[len(kwd):].strip().split()
-#                 return cmd, data
-#     return no_command, []
