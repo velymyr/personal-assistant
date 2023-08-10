15107eb579079306c3c78bf4c1b11de8c3f33da1
Rostyslav Bysko <brn.h74@gmail.com>
2023-08-10 13:47:57 +0300

    regular

--- address_book_classes.py
+++ address_book_classes.py
@@ -108,7 +108,7 @@ class Email(Field):
             else:
                 self.value = input("Email: ")
             try:
-                if re.match ("^[-a-z0-9!#$%&'*+/=?^_`{|}~]+(?:\.[-a-z0-9!#$%&'*+/=?^_`{|}~]+)*@(?:[a-z0-9]([-a-z0-9]{0,61}[a-z0-9])?\.)*(?:aero|arpa|asia|biz|cat|com|coop|edu|gov|info|int|jobs|mil|mobi|museum|name|net|org|pro|tel|travel|[a-z][a-z])$", self.value) or self.value == '':
+                if re.match (r"^[-a-z0-9!#$%&'*+/=?^_`{|}~]+(?:\.[-a-z0-9!#$%&'*+/=?^_`{|}~]+)*@(?:[a-z0-9]([-a-z0-9]{0,61}[a-z0-9])?\.)*(?:aero|arpa|asia|biz|cat|com|coop|edu|gov|info|int|jobs|mil|mobi|museum|name|net|org|pro|tel|travel|[a-z][a-z])$", self.value) or self.value == '':
                     break
                 else:
                     raise ValueError
