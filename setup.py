6eff9e149fe7a4e84c12b49357e0c461bf8d87f1
Volodymyr Hladkyi <volodymyr.hladkyi@gmail.com>
2023-08-10 16:45:25 +0300

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='Next Frontier Project',
-    version='0.1.7',
+    version='0.1.8',
     description='Personal Assistant Project',
     license='MIT',
     author='Next Frontier',
