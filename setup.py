934ac4e4a193b20bc5795ceef68f32c07b8783cb
Volodymyr Hladkyi <volodymyr.hladkyi@gmail.com>
2023-08-10 16:17:25 +0300

    Made it to compile... (#49)

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='Next Frontier Project',
+    version='0.1.7',
+    description='Personal Assistant Project',
+    license='MIT',
+    author='Next Frontier',
+    packages=['personal_assistant'],
+    install_requires=['rich'],
+    entry_points={
+        'console_scripts': [
+            'personal_assistant = personal_assistant.main_menu:menu'
+        ]
+    }
+)
